12cda207a1fea22b6227730dd2dc0752d6496f3d
hanine bouguerra <bouguerra.hanine@esprit.tn>
2023-12-05 21:13:20 +0100

    interfaces graphiques flutter

---
+++ lib/models/RecentFile.dart
@@ -0,0 +1,41 @@
+class RecentFile {
+  final String? id ,Nom, Adresse, Adresse_mail, telephone ,Role  ;
+
+  RecentFile( { this.id, this.Nom, this.Adresse, this .Adresse_mail ,this.telephone, this.Role,  });
+}
+
+List demoRecentFiles = [
+  RecentFile(
+  id: "1",
+  Nom: "Hanine Bouguerra",
+  Adresse: "Eljem",
+  Adresse_mail: "bouguerra.hanine@esprit.tn",
+  telephone: "93159203",
+  Role: "Client"
+  ),
+  RecentFile(
+   id: "2",
+   Nom: "Mariem",
+   Adresse: "Bardo-Tunis",
+   Adresse_mail: "mariem.marsaoui@esprit.tn",
+   telephone: "95463820",
+   Role: "Client"
+  ),
+ RecentFile(
+   id: "3",
+   Nom: "Futur génération",
+   Adresse: "Bardo-Tunis",
+   Adresse_mail: "mariem.marsaoui@esprit.tn",
+   telephone: "95463820",
+   Role: "Organisation"
+  ),
+  RecentFile(
+   id: "4",
+   Nom: "CRT ",
+   Adresse: "Tunis",
+   Adresse_mail: "crt.Tunis@gmail.com",
+   telephone: "95263820",
+   Role: "Organisation"
+  ),
+
+];
---
+++ lib/screens/main/components/side_menu.dart
@@ -0,0 +1,106 @@
+import 'package:admin/screens/dashboard/components/recent_files.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Image.asset("images/logo.png"),
+          ),
+          DrawerListTile(
+            title: "Overview",
+            svgSrc: "assets/icons/menu_dashboard.svg",
+            press: () {},
+          ),
+         DrawerListTile(
+  title: "Utilisateurs",
+  svgSrc: "assets/icons/menu_tran.svg",
+  press: () {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => RecentFiles()),
+    );
+  },
+),
+          DrawerListTile(
+            title: "Catégories",
+            svgSrc: "assets/icons/menu_task.svg",
+            press: () {},
+          ),
+          DrawerListTile(
+            title: "Articles",
+            svgSrc: "assets/icons/menu_doc.svg",
+            press: () {},
+          ),
+          DrawerListTile(
+            title: "Evenements",
+            svgSrc: "assets/icons/menu_store.svg",
+            press: () {},
+          ),
+          DrawerListTile(
+            title: "Livraison",
+            svgSrc: "assets/icons/menu_doc.svg",
+            press: () {},
+          ),
+          DrawerListTile(
+            title: "Réservation",
+            svgSrc: "assets/icons/menu_notification.svg",
+            press: () {},
+          ),
+          DrawerListTile(
+            title: "Panier",
+            svgSrc: "assets/icons/menu_notification.svg",
+            press: () {},
+          ),
+          DrawerListTile(
+            title: "Profile",
+            svgSrc: "assets/icons/menu_profile.svg",
+            press: () {},
+          ),
+          DrawerListTile(
+            title: "Settings",
+            svgSrc: "assets/icons/menu_setting.svg",
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DrawerListTile extends StatelessWidget {
+  const DrawerListTile({
+    Key? key,
+    required this.title,
+    required this.svgSrc,
+    required this.press,
+  }) : super(key: key);
+
+  final String title, svgSrc;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: press,
+      horizontalTitleGap: 0.0,
+      leading: SvgPicture.asset(
+        svgSrc,
+        colorFilter: ColorFilter.mode(Colors.white54, BlendMode.srcIn),
+        height: 16,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(color: Colors.white54),
+      ),
+    );
+  }
+}

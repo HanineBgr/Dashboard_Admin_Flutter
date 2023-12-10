2bb31359648e1ded21b872f8a5283ad67f561960
23karim <karim.kekli@esprit.tn>
2023-12-10 15:20:05 +0100

    Delet Point

--- lib/screens/dashboard/services/pc_services.dart
+++ lib/screens/dashboard/services/pc_services.dart
@@ -29,8 +29,7 @@ class PointCollecteService {
   }
 
   Future<PointCollecte> createPointCollecte(PointCollecte newPointCollecte) async {
-    final response = await http.post(
-      Uri.parse('http://localhost:5000/pointCollecte'),
+    final response = await http.post(Uri.parse('http://localhost:5000/pointCollecte'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -45,37 +44,3 @@ class PointCollecteService {
     }
   }
 }
-
-/*import 'dart:convert';
-import 'package:admin/models/PointCollecte.dart';
-import 'package:http/http.dart' as http;
-
-
-
-class PointCollecteService {
-  final String baseUrl;
-
-  PointCollecteService({required this.baseUrl});
-
-Future<List<PointCollecte>> getPointsCollecte() async {
-  final response = await http.get(Uri.parse('http://localhost:5000/pointCollecte'));
-
-  if (response.statusCode == 200) {
-    final List<dynamic> data = json.decode(response.body);
-    print(data); // Ajoutez cette ligne pour imprimer les données
-    return data.map((item) => PointCollecte.fromJson(item)).toList();
-  } else {
-    throw Exception('Erreur lors de la récupération des points de collecte');
-  }
-}
-
-  Future<void> deletePointCollecte(String id) async {
-    final response = await http.delete(Uri.parse('http://localhost:5000/pointCollecte/$id'));
-
-    if (response.statusCode == 200) {
-      print('Point de collecte supprimé avec succès');
-    } else {
-      throw Exception('Échec de la suppression du point de collecte');
-    }
-  }
-}*/

aa73dd899883fcd93cb2022f1e0d48e1c6542ffd
Aziz salem <aziz.salem@esprit.tn>
2023-12-22 15:36:01 +0100

    api Update & interface Update

--- lib/models/Reservation.dart
+++ lib/models/Reservation.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:translator/translator.dart';
 
 class ReservationPage extends StatefulWidget {
   @override
@@ -10,8 +11,8 @@ class ReservationPage extends StatefulWidget {
 class _ReservationPageState extends State<ReservationPage> {
   TextEditingController _searchController = TextEditingController();
   List<ReservationItem> allReservations = [];
-
   List<ReservationItem> displayedReservations = [];
+  String selectedLanguage = 'fr';
 
   @override
   void initState() {
@@ -38,6 +39,63 @@ class _ReservationPageState extends State<ReservationPage> {
     }
   }
 
+  Future<void> translateAllCommentaires() async {
+    final translator = GoogleTranslator();
+
+    for (int i = 0; i < allReservations.length; i++) {
+      Translation translation = await translator.translate(allReservations[i].commentaire, to: selectedLanguage);
+      await Future.delayed(Duration(milliseconds: 500)); // Ajout d'un délai de 500 ms
+      setState(() {
+        allReservations[i].commentaire = translation.text;
+      });
+    }
+  }
+
+  Future<void> _showLanguageSelectorPopup() async {
+    String selectedLanguage = 'fr'; // Default language is French
+
+    await showDialog<String>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Choisissez la Langue'),
+          content: DropdownButton<String>(
+            value: selectedLanguage,
+            items: [
+              DropdownMenuItem(
+                value: 'fr',
+                child: Text('Français'),
+              ),
+              DropdownMenuItem(
+                value: 'en',
+                child: Text('Anglais'),
+              ),
+              DropdownMenuItem(
+                value: 'es',
+                child: Text('Espagnol'),
+              ),
+              DropdownMenuItem(
+                value: 'ar',
+                child: Text('Arabe'),
+              ),
+            ],
+            onChanged: (value) {
+              selectedLanguage = value!;
+            },
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(selectedLanguage);
+              },
+              child: Text('Traduire'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,20 +118,79 @@ class _ReservationPageState extends State<ReservationPage> {
               ),
             ),
           ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: displayedReservations.length,
-              itemBuilder: (context, index) {
-                return displayedReservations[index];
-              },
-            ),
+          IconButton(
+            icon: Icon(Icons.translate),
+            onPressed: () async {
+              await _showLanguageSelectorPopup();
+              translateAllCommentaires();
+            },
           ),
         ],
       ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildLanguageButtonBar(),
+            SizedBox(height: 16),
+            Expanded(
+              child: ListView.builder(
+                itemCount: displayedReservations.length,
+                itemBuilder: (context, index) {
+                  return _buildReservationCard(displayedReservations[index]);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildReservationCard(ReservationItem reservation) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.symmetric(vertical: 8),
+      child: ListTile(
+        title: Text('ID de la Réservation: ${reservation.reservationId}'),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Date du Commentaire: ${reservation.dateCommentaire}'),
+            Text('Commentaire: ${reservation.commentaire}'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLanguageButtonBar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        _buildLanguageButton('Français', 'fr', Colors.green),
+        _buildLanguageButton('Anglais', 'en', Colors.blue),
+        _buildLanguageButton('Espagnol', 'es', Colors.red),
+        _buildLanguageButton('Arabe', 'ar', Colors.yellow),
+      ],
+    );
+  }
+
+  Widget _buildLanguageButton(String language, String languageCode, Color color) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: selectedLanguage == languageCode ? color : Colors.grey,
+        shadowColor: selectedLanguage == languageCode ? Colors.black : Colors.transparent,
+      ),
+      onPressed: () async {
+        await Future.delayed(Duration(milliseconds: 500)); // Ajout d'un délai de 500 ms
+        setState(() {
+          selectedLanguage = languageCode;
+        });
+        translateAllCommentaires();
+      },
+      child: Text(language),
     );
   }
 
@@ -88,12 +205,12 @@ class _ReservationPageState extends State<ReservationPage> {
   }
 }
 
-class ReservationItem extends StatelessWidget {
+class ReservationItem {
   final String reservationId;
   final String dateCommentaire;
-  final String commentaire;
+  String commentaire;
 
-  const ReservationItem({
+  ReservationItem({
     required this.reservationId,
     required this.dateCommentaire,
     required this.commentaire,
@@ -104,25 +221,6 @@ class ReservationItem extends StatelessWidget {
       reservationId: json['_id'],
       dateCommentaire: json['dateReservation'],
       commentaire: json['commentaire'],
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Divider(),
-        ListTile(
-          title: Text('ID de la Réservation: $reservationId'),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Date du Commentaire: $dateCommentaire'),
-              Text('Commentaire: $commentaire'),
-            ],
-          ),
-        ),
-      ],
     );
   }
 }
